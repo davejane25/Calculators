e2bf27b3e03063f2ea5e520e52f8d5f7f7fac3bf
Arunkumar-60 <84034637+Arunkumar-60@users.noreply.github.com>
2022-10-23 12:07:20 +0530

    AGE - calculator responsive layout completed

--- Age-calculator/index.js
+++ Age-calculator/index.js
@@ -20,6 +20,9 @@ function age() {
     var d = d2 - d1;
     var m = m2 - m1;
     var y = y2 - y1;
-  
+  if (y<0) {
+    document.getElementById('age').innerHTML = 'enter valid date of birth';
+  } else {
     document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+  }
 }
